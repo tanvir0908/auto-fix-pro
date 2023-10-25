50c5e10234ba8c9b18bc7671176f2e63b3303a88
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-10-25 17:56:49 +0600

    Responsive footer completed

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -7,17 +7,17 @@ import {
 
 export default function Footer() {
   return (
-    <div className="bg-[#151515] py-32 xl:px-32 lg:px-12 justify-evenly gap-5 md:px-8 px-4 text-white grid md:grid-cols-2 lg:grid-cols-4 ">
+    <div className="bg-[#151515] py-32 xl:px-32 lg:px-20 md:px-16 px-5  text-center md:text-left md:justify-evenly gap-12 md:gap-20 lg:gap-5  text-white grid md:grid-cols-2 lg:grid-cols-4 ">
       <div className="">
-        <div className="text-primary flex items-center gap-2 font-secondary">
-          <BsFillCarFrontFill size={40} />
-          <p className="text-3xl font-semibold">AutoFix Pro</p>
+        <div className="text-primary flex items-center mx-auto gap-2 justify-center md:justify-start font-secondary">
+          <BsFillCarFrontFill className="text-[2rem] xl:text-[3rem]" />
+          <p className="text-2xl xl:text-3xl font-semibold">AutoFix Pro</p>
         </div>
-        <p className="text-lg font-medium my-5">
+        <p className="xl:text-lg leading-loose xl:leading-loose font-medium my-5">
           {`Your trusted partner in automotive excellence. We're dedicated to
           keeping your ride running smoothly and safely.`}
         </p>
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3 xl:gap-5 justify-center md:justify-start">
           <AiOutlineGoogle
             className=" text-white bg-text-secondary p-2 rounded-full"
             size={40}
@@ -37,23 +37,22 @@ export default function Footer() {
           />
         </div>
       </div>
-      <div className="mx-auto">
-        <h2 className="text-2xl font-semibold mb-5">About</h2>
-        <p className="text-lg font-medium">Home</p>
-        <p className="text-lg font-medium my-2">Service</p>
-        <p className="text-lg font-medium">Contact</p>
+      <div className="lg:mx-auto">
+        <h2 className="text-xl xl:text-2xl font-semibold mb-5">About</h2>
+        <p className="xl:text-lg font-medium">Home</p>
+        <p className="xl:text-lg font-medium my-2">Service</p>
+        <p className="xl:text-lg font-medium">Contact</p>
       </div>
-      <div className="mx-auto">
-        <h2 className="text-2xl font-semibold mb-5">About</h2>
-        <p className="text-lg font-medium">Home</p>
-        <p className="text-lg font-medium my-2">Service</p>
-        <p className="text-lg font-medium">Contact</p>
+      <div className="lg:mx-auto">
+        <h2 className="text-xl xl:text-2xl font-semibold mb-5">Company</h2>
+        <p className="xl:text-lg font-medium">Why AutoFix Pro</p>
+        <p className="xl:text-lg font-medium my-2">About</p>
       </div>
-      <div className="mx-auto">
-        <h2 className="text-2xl font-semibold mb-5">About</h2>
-        <p className="text-lg font-medium">Home</p>
-        <p className="text-lg font-medium my-2">Service</p>
-        <p className="text-lg font-medium">Contact</p>
+      <div className="lg:mx-auto">
+        <h2 className="text-xl xl:text-2xl font-semibold mb-5">Support</h2>
+        <p className="xl:text-lg font-medium">Support Center</p>
+        <p className="xl:text-lg font-medium my-2">Feedback</p>
+        <p className="xl:text-lg font-medium">Accesbility</p>
       </div>
     </div>
   );
